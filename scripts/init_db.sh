907e6fba38722afd6e674681e097cc301ca3f438
Martin Kiefel <mrtrac@nopw.de>
2014-09-29 23:37:19 +0200

    Makes init_db callable from inside the container.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,15 +3,13 @@
 # This script sets up directories and fixes permissions
 #
 
-# find the scripts directory (note the /..)
-DIR="$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+# find the scripts directory
+DIR="$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/load_config.sh
 cd "$REPO_DIR"
 
-IP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' openpose-data)
-PORT=$(sudo docker inspect --format '{{ .Config.ExposedPorts }}' openpose-data | sed 's#^[^[:digit:]]*\([[:digit:]]*\)/tcp.*$#\1#')
-echo "CREATE USER \"${DB_USER}\" WITH LOGIN PASSWORD '${DB_PASS}'" | psql -U postgres -h "${IP}" -p "${PORT}" -U postgres
-createdb -U postgres -h "${IP}" -p "${PORT}" -U postgres --owner=labelmaterial --encoding=UTF8 labelmaterial
+echo "CREATE USER \"${DB_USER}\" WITH LOGIN PASSWORD '${DB_PASS}'" | psql -U postgres -h "${DB_HOST}" -p "${DB_PORT}" -U postgres
+createdb -U postgres -h "${IP}" -p "${PORT}" -U postgres --owner=${DB_USER} --encoding=UTF8 ${DB_NAME}
 
 python ./server/manage.py syncdb
 python ./server/manage.py migrate
