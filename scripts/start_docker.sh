f56ef233538c4b88a8c7d33fc64088a3b7304465
Martin Kiefel <mrtrac@nopw.de>
2014-09-30 22:56:26 +0200

    Removes container removal.

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -8,5 +8,5 @@ DIR="$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/load_config.sh
 cd "$REPO_DIR"
 
-sudo docker run -t -i --rm -p 45001:80 --link openpose-data:db \
+sudo docker run -t -i -p 45001:80 --link openpose-data:db \
   -v ${REPO_DIR}/media:/home/appuser/data/media openpose /bin/bash
