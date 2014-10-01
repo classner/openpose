d205ddddeba6ccd5d5f7094abc4918362758baad
Martin Kiefel <mrtrac@nopw.de>
2014-10-01 15:06:27 +0200

    Fixes types in init_db.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -9,7 +9,7 @@ source $DIR/load_config.sh
 cd "$REPO_DIR"
 
 echo "CREATE USER \"${DB_USER}\" WITH LOGIN PASSWORD '${DB_PASS}'" | psql -U postgres -h "${DB_HOST}" -p "${DB_PORT}" -U postgres
-createdb -U postgres -h "${IP}" -p "${PORT}" -U postgres --owner=${DB_USER} --encoding=UTF8 ${DB_NAME}
+createdb -U postgres -h "${DB_HOST}" -p "${DB_PORT}" -U postgres --owner=${DB_USER} --encoding=UTF8 ${DB_NAME}
 
 python ./server/manage.py syncdb
 python ./server/manage.py migrate
