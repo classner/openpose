21e99cbaa0e6374196a1854f35cf60816798a0c2
Martin Kiefel <mrtrac@nopw.de>
2014-10-14 18:07:11 +0200

    Adds tutorial flag to segmentation experiment.

--- server/segmentation/experiments.py
+++ server/segmentation/experiments.py
@@ -37,7 +37,8 @@ def configure_experiments():
         keywords='person,images,segment',
         #frame_height=8000,
         requirements={},
-        auto_add_hits=False,  # settings.MTURK_SANDBOX,
+        auto_add_hits=False,
+        has_tutorial=True,
     )
 
 
