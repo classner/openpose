7147f15d53f70ca457f012a263207fd75e1bc568
Martin Kiefel <mrtrac@nopw.de>
2015-03-31 19:24:27 +0200

    Adds random ordering of images in dataset.

--- server/segmentation/management/commands/extract_dataset.py
+++ server/segmentation/management/commands/extract_dataset.py
@@ -1,4 +1,5 @@
-import re
+import random
+import string
 import sys
 import os.path
 from random import choice
@@ -14,11 +15,16 @@ import cv2
 
 import lmdb
 
-from photos.models import PhotoDataset
+from photos.models import Photo
 
 
 class Command(BaseCommand):
     help = 'Extract a dataset'
+
+    def random_key_(self):
+        return ''.join(
+            random.choice(string.ascii_uppercase + string.digits)
+            for _ in range(6))
 
     def write_segmentation_(self, caffe, photo, person, tasks, txn):
         # gather all those good segmentations
@@ -76,15 +82,20 @@ class Command(BaseCommand):
         image_file.close()
         photo.image_orig.close()
 
-        input = np.concatenate((image, cleaned_segmentation[:, :, None]),
-                               axis=2)
+        input = np.transpose(
+            np.concatenate((image, cleaned_segmentation[:, :, None]),
+                           axis=2),
+            [2, 0, 1])
+
         datum = caffe.io.array_to_datum(input)
-        txn.put(str(photo.name), datum.SerializeToString(), overwrite=False)
+        txn.put(self.random_key_() + str(photo.name),
+                datum.SerializeToString(),
+                overwrite=False)
 
     def handle(self, *args, **options):
         caffe_root = args[0]
-        dataset_name = args[0]
-        include_regex = args[2]
+        dataset_name = args[1]
+        include_list_path = args[2]
         db_path = args[3]
 
         # Load caffe
@@ -96,38 +107,35 @@ class Command(BaseCommand):
         txn = db.begin(write=True)
         transactions = 0
 
-        photos = PhotoDataset.objects.get(name=dataset_name).photos.all()
+        with open(include_list_path, 'r') as list_file:
+            for photo_name in progress.bar(list_file.readlines()):
+                photo_name = photo_name.rstrip()
+                photo = Photo.objects.get(dataset__name=dataset_name,
+                                          name=photo_name)
 
-        include = re.compile(include_regex)
+                # XXX for now we'll assume that we have one person per image
+                assert(photo.persons.count() == 1)
 
-        # walk through all images and write out a correct person segmentation
-        for photo in progress.bar(photos):
-            # XXX for now we'll assume that we have one person per image
-            assert(photo.persons.count() == 1)
+                person = photo.persons.all()[0]
+                tasks = person.segmentation_tasks.filter(
+                    # full body segmentation
+                    part__isnull=True,
+                    # at least one person thinks this is correct
+                    responses__qualities__correct=True,
+                )
 
-            if not include.search(photo.name):
-                continue
+                if not tasks:
+                    print('exclude: {}'.format(photo.name))
+                    continue
 
-            person = photo.persons.all()[0]
-            tasks = person.segmentation_tasks.filter(
-                # full body segmentation
-                part__isnull=True,
-                # at least one person thinks this is correct
-                responses__qualities__correct=True,
-            )
+                self.write_segmentation_(caffe, photo, person, tasks, txn)
 
-            if not tasks:
-                print('exclude: {}'.format(photo.name))
-                continue
+                transactions += 1
 
-            self.write_segmentation_(caffe, photo, person, tasks, txn)
-
-            transactions += 1
-
-            if transactions > 300:
-                txn.commit()
-                transactions = 0
-                txn = db.begin(write=True)
+                if transactions > 300:
+                    txn.commit()
+                    transactions = 0
+                    txn = db.begin(write=True)
 
         if transactions > 0:
             txn.commit()
