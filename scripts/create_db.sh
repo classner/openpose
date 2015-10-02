ae2c0af9bf7b974afeaefb1098479c8b2e333548
Christoph Lassner <mail@christophlassner.de>
2015-10-02 13:10:11 +0200

    Small adjustments.

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -4,8 +4,9 @@
 #
 
 # find the scripts directory
-DIR="$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $DIR/load_config.sh
+# DIR="$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#source $DIR/load_config.sh
+source scripts/load_config.sh
 cd "$REPO_DIR"
-
+mkdir -p ${DB_DIR}
 sudo docker run --name "${PROJECT_NAME}-data" -v ${DB_DIR}:/var/lib/postgresql/data -d postgres
--- scripts/install/build_docker.sh
+++ scripts/install/build_docker.sh
@@ -7,18 +7,16 @@
 
 # load configuration info
 set -e
-DIR="$( builtin cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )/.." && pwd )"
-source "$DIR/load_config.sh"
+source "scripts/load_config.sh"
 
 echo ""
 echo "===================================================================="
 echo "Now creating docker image."
 REV=`git rev-parse HEAD`
-sudo docker build -t "${PROJECT_NAME}:${REV}" .
-sudo docker tag "${PROJECT_NAME}:${REV}" "${PROJECT_NAME}:latest"
+docker build -t "${PROJECT_NAME}:${REV}" .
+docker tag "${PROJECT_NAME}:${REV}" "${PROJECT_NAME}:latest"
 
 # exit message
 echo "$0: done!"
 echo ""
 echo "===================================================================="
-
