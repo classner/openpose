76cc1f0f4347b4d9bbe5af60785dff223b6ddf24
Christoph Lassner <mail@christophlassner.de>
2015-10-06 19:11:32 +0200

    Reworked the create_part_task.

--- server/segmentation/management/commands/create_part_task.py
+++ server/segmentation/management/commands/create_part_task.py
@@ -1,28 +1,27 @@
-import os
-from optparse import make_option
-
-from clint.textui import progress
+"""Part annotation related task."""
+# pylint: disable=F0401, R0201, R0903
+from clint.textui import progress, puts, colored
 
 from django.core.management.base import BaseCommand
 from accounts.models import UserProfile
 
-import numpy as np
-import scipy.io
-
-import json
-
 from pose.models import Person
 
-class Command(BaseCommand):
+
+class Command(BaseCommand):  # pylint: disable=W0232
+
+    """Command implementation."""
+
     args = '<user> <dataset> [<part>]'
-    help = 'Create segmentation tasks. If part is not given a full segmentation is created.'
+    help = ('Create segmentation tasks. If part is not given a full '
+            'segmentation is created.')
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **options):  # pylint: disable=W0613
+        """Implementation."""
         dataset_name = args[1]
-
-
         if len(args) < 2:
-            print 'Please supply a dataset name and optionally a part name.'
+            puts(colored.red('Please supply a dataset name and optionally a '
+                             'part name.'))
             return
         else:
             username = args[0]
@@ -32,28 +31,28 @@ class Command(BaseCommand):
             if len(args) == 3:
                 part = args[2]
             else:
-                print 'Too many arguments.'
+                puts(colored.red('Too many arguments.'))
                 return
 
         user = UserProfile.objects.get(user__username=username)
-
         persons = Person.objects.filter(photo__dataset__name=dataset_name)
 
         created_count = 0
-
-        for p in progress.bar(persons):
+        for person in progress.bar(persons):
             # check if this person has a proper segmentation
-            correct_full_tasks = p.segmentation_tasks.filter(
-                    part__isnull=True,
-                    responses__qualities__correct=True).count()
+            correct_full_tasks = person.segmentation_tasks.filter(
+                part__isnull=True,
+                responses__qualities__correct=True).count()
 
             if correct_full_tasks > 0:
                 # check if there is not already a part task that we would like
                 # to create
-                task, created = p.segmentation_tasks.get_or_create(
-                        parse_pose=p.parse_poses.all()[0], part=part)
+                _, created = person.segmentation_tasks.get_or_create(
+                    user=user,
+                    parse_pose=person.parse_poses.all()[0],
+                    part=part)
 
                 if created:
                     created_count += 1
 
-        print('created {} new tasks'.format(created_count))
+        puts('created {} new tasks'.format(created_count))
